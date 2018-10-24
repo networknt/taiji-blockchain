f4447d525691dc37cf1d0730aab065c83a747b3a
Steve Hu <stevehu@gmail.com>
2018-10-24 15:45:09 -0400

    fixes #15 add ledge id which is the on chain unique identifier to LedgeEntry

--- taiji-crypto/src/main/java/com/networknt/taiji/crypto/LedgerEntry.java
+++ taiji-crypto/src/main/java/com/networknt/taiji/crypto/LedgerEntry.java
@@ -13,6 +13,7 @@ import com.networknt.chain.utility.Numeric;
  * @author Steve Hu
  */
 public class LedgerEntry {
+    long id;              // generated ledger id once it is on the chain
     String toAddress;     // the credit address or account
     long value;           // debit amount for the fromAddress in shell
     String data;          // transaction data or smart contract data
@@ -25,6 +26,14 @@ public class LedgerEntry {
 
     public LedgerEntry(String toAddress, long value) {
         this(toAddress, value, "");
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getToAddress() {
