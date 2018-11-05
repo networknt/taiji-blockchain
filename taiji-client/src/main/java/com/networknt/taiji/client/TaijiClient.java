2d7036755238c1ff139ba02555059faf00d27f12
Steve Hu <stevehu@gmail.com>
2018-11-04 20:05:59 -0500

    fixes #19 add getSnapshot method for chain-reader access in TaijiClient

--- taiji-client/src/main/java/com/networknt/taiji/client/TaijiClient.java
+++ taiji-client/src/main/java/com/networknt/taiji/client/TaijiClient.java
@@ -1,8 +1,10 @@
 package com.networknt.taiji.client;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.networknt.client.Http2Client;
 import com.networknt.cluster.Cluster;
 import com.networknt.config.Config;
+import com.networknt.exception.ApiException;
 import com.networknt.service.SingletonServiceFactory;
 import com.networknt.status.Status;
 import com.networknt.taiji.crypto.SignedTransaction;
@@ -18,24 +20,41 @@ import org.slf4j.LoggerFactory;
 import org.xnio.OptionMap;
 
 import java.net.URI;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 
+/**
+ * This is the client that is responsible for service discovery and interact with
+ * chain-writer and chain-reader services. It uses Consul for service lookup with
+ * the bankId.
+ *
+ * @author Steve Hu
+ */
 public class TaijiClient {
     static final Logger logger = LoggerFactory.getLogger(TaijiClient.class);
     // service name
-    static final String serviceId = "com.networknt.chainwriter-1.0.0";
+    static final String writerServiceId = "com.networknt.chainwriter-1.0.0";
+    static final String readerServiceId = "com.networknt.chainreader-1.0.0";
     // Get the singleton Cluster instance
     static Cluster cluster = SingletonServiceFactory.getBean(Cluster.class);
     // Get the singleton Http2Client instance
     static Http2Client client = Http2Client.getInstance();
 
+    /**
+     * This is the API to write a single transaction to the Taiji blockchain.
+     *
+     * @param bankId The first 4 digits of the address
+     * @param stx Signed transaction
+     * @return Status
+     */
     public static Status postTx(String bankId, SignedTransaction stx) {
         Status status = null;
         // host name or IP address
-        String apiHost = cluster.serviceToUrl("https", serviceId, bankId, null);
+        String apiHost = cluster.serviceToUrl("https", writerServiceId, bankId, null);
         try {
             String requestBody = Config.getInstance().getMapper().writeValueAsString(stx);
             logger.debug("requestBody = " + requestBody);
@@ -65,10 +84,17 @@ public class TaijiClient {
         return status;
     }
 
+    /**
+     * This is the API to write multiple transactions to the Taiji blockchain in batch.
+     *
+     * @param bankId The first 4 digits of the address
+     * @param stxs A list of signed transactions.
+     * @return Status
+     */
     public static Status postTxs(String bankId, List<SignedTransaction> stxs) {
         Status status = null;
         // host name or IP address
-        String apiHost = cluster.serviceToUrl("https", serviceId, bankId, null);
+        String apiHost = cluster.serviceToUrl("https", writerServiceId, bankId, null);
 
         try {
             String requestBody = Config.getInstance().getMapper().writeValueAsString(stxs);
@@ -98,4 +124,42 @@ public class TaijiClient {
         }
         return status;
     }
+
+    /**
+     * Get the balance snapshot for the address from the chain-reader.
+     *
+     * @param address currency address
+     * @return Map of current and balance
+     * @throws ApiException wrap up any exception status in this ApiException
+     */
+
+    public static Map<String, Long> getSnapshot(String address) throws ApiException {
+        Map<String, Long> currencyMap = null;
+        // host name or IP address
+        String apiHost = cluster.serviceToUrl("https", readerServiceId, address.substring(0, 4), null);
+        try {
+            // This is a connection that is shared by multiple requests and won't close until the app exits.
+            ClientConnection connection = client.connect(new URI(apiHost), Http2Client.WORKER, Http2Client.SSL, Http2Client.BUFFER_POOL, OptionMap.create(UndertowOptions.ENABLE_HTTP2, true)).get();
+            // Create one CountDownLatch that will be reset in the callback function
+            final CountDownLatch latch = new CountDownLatch(1);
+            // Create an AtomicReference object to receive ClientResponse from callback function
+            final AtomicReference<ClientResponse> reference = new AtomicReference<>();
+            final ClientRequest request = new ClientRequest().setMethod(Methods.GET).setPath("/account/" + address);
+            request.getRequestHeaders().put(Headers.HOST, "localhost");
+            connection.sendRequest(request, client.createClientCallback(reference, latch));
+            latch.await();
+            int statusCode = reference.get().getResponseCode();
+            String body = reference.get().getAttachment(Http2Client.RESPONSE_BODY);
+            if(statusCode != 200) {
+                Status status = Config.getInstance().getMapper().readValue(body, Status.class);
+                throw new ApiException(status);
+            } else {
+                currencyMap = Config.getInstance().getMapper().readValue(body, new TypeReference<HashMap<String,Long>>() {});
+            }
+        } catch (Exception e) {
+            logger.error("Exception:", e);
+        }
+        return currencyMap;
+    }
+
 }
