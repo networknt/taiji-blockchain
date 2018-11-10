ccf40ba94731d35e22d526d41394283a8e18f7d1
Steve Hu <stevehu@gmail.com>
2018-11-10 16:32:14 -0500

    fixes #23 upgrade to light-4j 1.5.22

--- taiji-client/build.gradle.kts
+++ taiji-client/build.gradle.kts
@@ -7,11 +7,11 @@ plugins {
 dependencies {
     compile(project(":taiji-core"))
     compile(project(":taiji-crypto"))
-    compile("com.networknt:client:1.5.22-SNAPSHOT")
-    compile("com.networknt:cluster:1.5.22-SNAPSHOT")
-    compile("com.networknt:balance:1.5.22-SNAPSHOT")
-    compile("com.networknt:consul:1.5.22-SNAPSHOT")
-    compile("com.networknt:monad-result:1.5.22-SNAPSHOT")
+    compile("com.networknt:client:1.5.22")
+    compile("com.networknt:cluster:1.5.22")
+    compile("com.networknt:balance:1.5.22")
+    compile("com.networknt:consul:1.5.22")
+    compile("com.networknt:monad-result:1.5.22")
     compile("com.beust:jcommander:1.72")
     compile("ch.qos.logback:logback-classic:1.2.3")
 }
