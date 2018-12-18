095604bcf57497621f3ab84f9b860f4509ee999f
Steve Hu <stevehu@gmail.com>
2018-12-17 19:18:30 -0500

    fixes #54 add taiji-kyc module for know your client per address

---
+++ taiji-kyc/src/main/avro/compile.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# -string is very necessary, otherwise the key of map will be Utf8 internal class. You cannot find object with string.
+
+java -jar /home/steve/tool/avro-tools-1.8.2.jar compile -string schema KycCreatedEvent.avsc .
+java -jar /home/steve/tool/avro-tools-1.8.2.jar compile -string schema KycVerifiedEvent.avsc .
+
+# Update events to implement from KycEvent interface in order to group these events in streams processing.
+
+find . -name '*Event.java' -exec sed -i "s/implements org.apache.avro.specific.SpecificRecord/implements KycEvent/g" {} +
+
+# move to the right directory and remove the generated folder.
+
+mv com/networknt/taiji/kyc/KycCreatedEvent.java ../java/com/networknt/taiji/kyc
+mv com/networknt/taiji/kyc/KycVerifiedEvent.java ../java/com/networknt/taiji/kyc
+mv com/networknt/taiji/kyc/KycType.java ../java/com/networknt/taiji/kyc
+
+rm -rf com
---
+++ taiji-kyc/src/main/java/com/networknt/taiji/kyc/KycEvent.java
@@ -0,0 +1,6 @@
+package com.networknt.taiji.kyc;
+
+import com.networknt.taiji.event.Event;
+
+public interface KycEvent extends Event, org.apache.avro.specific.SpecificRecord {
+}
